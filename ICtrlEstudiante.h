3617f78c0fa902730851fd9689a120d860336751
Juan Pablo <juanpim2000@gmail.com>
2015-06-12 19:10:00 -0300

    CtrlEstudiante

--- ICtrlEstudiante.h
+++ ICtrlEstudiante.h
@@ -8,11 +8,32 @@
 #ifndef ICTRLESTUDIANTE_H_
 #define ICTRLESTUDIANTE_H_
 
+#include <set>
+#include <string>
+
+#include "DTEstudiante.h"
+#include "DataEstudiante.h"
+#include "Date.h"
+//#include "DTSeccion.h"
+//#include "DataOferta.h"
+//
+
 using namespace std;
 
 class ICtrlEstudiantes {
 public:
-	virtual ~ICtrlEstudiantes() {};
+	virtual set<DTEstudiante> listarEstudiantes() = 0;
+	virtual bool seleccionarEstudiante(string CI) = 0;
+	virtual DataEstudiante consultarDatosEstudiante() = 0;
+	virtual void modDatosEstudiante(string nom, string ape, Date* fechaNac, int tel) = 0;
+	virtual void addCarrera(string cod) = 0;
+	virtual void quitCarrera(string cod) = 0;
+	virtual void addAsignatura(string cod, Date* fecha, int ) = 0;
+	virtual void quitAsignatura(string cod) = 0;
+
+	virtual void mostrarNotificaciones() = 0;
+
+
 };
 
 #endif /* ICTRLESTUDIANTE_H_ */
